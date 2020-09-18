1b68309f7566655b4b57bd495325bc1d5e404492
Rahul Verma <webgrowthhub@gmail.com>
2020-09-18 23:22:45 +0530

    fdsfdsfsdfdfd

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var AllModel=require("./modules/database");
 const cron = require("node-cron");
 const querystring = require('querystring');
 const expressip = require('express-ip');
+const moment = require('moment');
 
 
 var app = express();
@@ -30,74 +31,74 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 
-// cron.schedule("*/6 * * * * ", function() {
-//   var getroww33=AllModel.TotalMobilerecords.find({_id : "5f0d9da752e91a0b3c4fb864"});
-//   getroww33.exec((errorr22,TotalrecodDtataa3)=>{
-//     var myDate = new Date(); 
-//     var registerdate=myDate.getDate();  
-// var registermonth=myDate.getMonth()+1;  
-// var registeryear=myDate.getFullYear(); 
-// var Datee= registerdate + "-" + registermonth + "-" +  registeryear;
+cron.schedule("*/6 * * * * ", function() {
+  var getroww33=AllModel.TotalMobilerecords.find({_id : "5f0d9da752e91a0b3c4fb864"});
+  getroww33.exec((errorr22,TotalrecodDtataa3)=>{
+    var myDate = new Date(); 
+    var registerdate=myDate.getDate();  
+var registermonth=myDate.getMonth()+1;  
+var registeryear=myDate.getFullYear(); 
+var Datee= registerdate + "-" + registermonth + "-" +  registeryear;
 
-//     if(TotalrecodDtataa3[0].currentdate == Datee){
-//       var getroww=AllModel.TotalMobilerecords.find({_id : "5f0d9da752e91a0b3c4fb864"});
-//       getroww.exec((errorr,TotalrecodDtat)=>{
-//         if(TotalrecodDtat[0].currentoffset <= TotalrecodDtat[0].totalreacords){
-//           console.log(TotalrecodDtat[0].currentoffset);
-//            request('https://api.ftc.gov/v0/dnc-complaints?api_key=gWj2iYA256KcL1c0i778JRUvCRp7pBtOdBBfAhg2&items_per_page=50&offset='+TotalrecodDtat[0].currentoffset, { json: true }, (err, ress2, body) => {
-//             // if (err) { return console.log(err); }
-//             var i;
-//              if(ress2.body.data){
-//             for(var i=0; i<50; i++){
+    if(TotalrecodDtataa3[0].currentdate == Datee){
+      var getroww=AllModel.TotalMobilerecords.find({_id : "5f0d9da752e91a0b3c4fb864"});
+      getroww.exec((errorr,TotalrecodDtat)=>{
+        if(TotalrecodDtat[0].currentoffset <= TotalrecodDtat[0].totalreacords){
+          console.log(TotalrecodDtat[0].currentoffset);
+           request('https://api.ftc.gov/v0/dnc-complaints?api_key=gWj2iYA256KcL1c0i778JRUvCRp7pBtOdBBfAhg2&items_per_page=50&offset='+TotalrecodDtat[0].currentoffset, { json: true }, (err, ress2, body) => {
+            // if (err) { return console.log(err); }
+            var i;
+             if(ress2.body.data){
+            for(var i=0; i<50; i++){
    
-//             var NewImage=new AllModel.Mobilerecords({
-//               complaintid : ress2.body.data[i]['id'],
-//          companynumber : ress2.body.data[i]['attributes']['company-phone-number'],
-//           createddate : ress2.body.data[i]['attributes']['created-date'],
+            var NewImage=new AllModel.Mobilerecords({
+              complaintid : ress2.body.data[i]['id'],
+         companynumber : ress2.body.data[i]['attributes']['company-phone-number'],
+          createddate : ress2.body.data[i]['attributes']['created-date'],
              
-//             });
-//             NewImage.save((err,saved)=>{})
+            });
+            NewImage.save((err,saved)=>{})
           
-//           }
-//           var update_q2=AllModel.TotalMobilerecords.updateOne({_id : "5f0d9da752e91a0b3c4fb864"},{currentoffset : TotalrecodDtat[0].currentoffset+50});
+          }
+          var update_q2=AllModel.TotalMobilerecords.updateOne({_id : "5f0d9da752e91a0b3c4fb864"},{currentoffset : TotalrecodDtat[0].currentoffset+50});
 
-//           update_q2.exec();
+          update_q2.exec();
        
-//       }
+      }
     
-//         });
-//         }
+        });
+        }
 
  
-//       })
-//     }else{
+      })
+    }else{
 
       
-//   request('https://api.ftc.gov/v0/dnc-complaints?api_key=gWj2iYA256KcL1c0i778JRUvCRp7pBtOdBBfAhg2', { json: true }, (err, ress, body) => {
-//     if (err) { return console.log(err); }
-//     var totol=ress.body.meta["record-total"];
+  request('https://api.ftc.gov/v0/dnc-complaints?api_key=gWj2iYA256KcL1c0i778JRUvCRp7pBtOdBBfAhg2', { json: true }, (err, ress, body) => {
+    if (err) { return console.log(err); }
+    var totol=ress.body.meta["record-total"];
 
-//     var myDate = new Date(); 
-//                 var registerdate=myDate.getDate();  
-//       var registermonth=myDate.getMonth()+1;  
-//       var registeryear=myDate.getFullYear(); 
-//       var Datee= registerdate + "-" + registermonth + "-" +  registeryear;
+    var myDate = new Date(); 
+                var registerdate=myDate.getDate();  
+      var registermonth=myDate.getMonth()+1;  
+      var registeryear=myDate.getFullYear(); 
+      var Datee= registerdate + "-" + registermonth + "-" +  registeryear;
 
-//     var update_q=AllModel.TotalMobilerecords.updateOne({_id : "5f0d9da752e91a0b3c4fb864"},{totalreacords : totol , currentdate : Datee});
-//     update_q.exec((err,Updated)=>{
-//       if(Updated){
+    var update_q=AllModel.TotalMobilerecords.updateOne({_id : "5f0d9da752e91a0b3c4fb864"},{totalreacords : totol , currentdate : Datee});
+    update_q.exec((err,Updated)=>{
+      if(Updated){
      
       
-//       }
-//     })
+      }
+    })
    
-// });
-//     }
+});
+    }
 
 
-//   })
+  })
 
-// });
+});
 
 /* GET home page. */
 app.get('/', function(req, res, next) {
@@ -112,7 +113,11 @@ app.get('/', function(req, res, next) {
   });
 
   app.get('/check', function(req, res, next) {
-    res.send(req.ipInfo);
+  var   currentIp=req.ipInfo.ip;
+    var IpAddress=currentIp.split('-')[3];
+    var city=req.ipInfo.city;
+    var country=req.ipInfo.country;
+    // console.log(moment('2020-09-18 14:59:32').fromNow());
   });
   
   
@@ -149,7 +154,7 @@ app.get('/', function(req, res, next) {
   app.post('/get_results', function(req, res, next) {
 
   var phon_no=req.body.phone_no;
-  var get_res=AllModel.Mobilerecords.find({ 'companynumber': {'$regex': phon_no} });
+  var get_res=AllModel.Mobilerecords.find({ 'companynumber': {'$regex': phon_no} }).limit(15);
   get_res.exec((err,data)=>{
     if(data[0]){
       res.send(data);
